078022bf51096d924c0193bf4587b6cb53b17370
hyago <hyago@pop-os.localdomain>
2023-07-18 06:56:19 -0300

    FEAT/ Thumb detection + Orientation Detection

--- labeloverlay.m
+++
@@ -1,5 +0,0 @@
-function B = labeloverlay(A, L)
-    cmap = jet(max(L(:)));  % Generate a colormap for different labels
-    rgb = ind2rgb(L, cmap);  % Convert label matrix to RGB image
-    B = imoverlay(A, rgb);   % Overlay the RGB image onto the original image
-end
